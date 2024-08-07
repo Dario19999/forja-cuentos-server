cd4f541511a25e816685e33fd1d5b5f3ce0a1309
Dario <dario_gow3@hotmail.com>
2024-08-07 02:35:02 -0600

    User controller

---
+++ controllers/User.js
@@ -0,0 +1,43 @@
+const getUser = (req, res) => {    
+    res.json({
+        msg: 'get API'
+    });
+}
+
+const createUser = (req, res) => {    
+    res.json({
+        msg: 'post API'
+    });
+}
+
+const updateUser = (req, res) => {        
+    const id = req.params.userId;
+    res.json({
+        msg: 'put API',
+        id
+    });
+}
+
+const deleteUser = (req, res) => {
+    const id = req.params.userId;
+    res.json({
+        msg: 'delete API',
+        id
+    });
+}
+
+const notFound = (req, res) => {
+    const id = req.params.userId;
+    res.status(404).json({
+        msg: '404 - Not Found',
+        id
+    });
+}
+
+module.exports = {
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    notFound
+}
