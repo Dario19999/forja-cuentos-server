6bdeb2f16cd169cb34bd4f0740f437b65e4cbd84
Dario <dario_gow3@hotmail.com>
2024-08-14 01:34:16 -0600

    Sequelize db connection

--- models/server.js
+++ models/server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-
+const { sequelize } = require('../database/config');
 class Server {
     constructor() {
         this.express = express();
         this.port = process.env.PORT;
+
+        //Connect to database
+        this.connectDB();
         //Middlewares
         this.middleware();
         //Routes
         this.routes();
         
+    }
+
+    async connectDB() {
+        await sequelize.authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+        });
     }
 
     middleware() {
